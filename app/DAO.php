7bf884259e4c7ed99c54883f8d7bc420dac863e2
Anthony <semprini.anthony@gmail.com>
2023-07-12 10:08:50 +0200

    mise en place de 2 system de connexion pdo

--- app/DAO.php
+++ app/DAO.php
@@ -11,11 +11,17 @@
      * @method static select() requètes de sélection
      */
     abstract class DAO{
-
-        private static $host   = 'mysql:host=127.0.0.1;port=8889';
-        private static $dbname = 'forum_dwwm3';
+        // heidiSql
+        private static $host   = 'mysql:host=127.0.0.1;port=3306';
+        private static $dbname = 'forum';
         private static $dbuser = 'root';
-        private static $dbpass = 'root';
+        private static $dbpass = '';
+
+        // php myAdmin
+        // private static $host   = 'mysql:host=127.0.0.1;port=8889';
+        // private static $dbname = 'forum';
+        // private static $dbuser = 'root';
+        // private static $dbpass = 'root';
 
         private static $bdd;
 
