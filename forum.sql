fe7b2b25093e174388e62145a7c4b995f64f9407
Anthony <semprini.anthony@gmail.com>
2023-07-17 09:32:33 +0200

    mise a jour bdd

--- forum.sql
+++ forum.sql
@@ -21,10 +21,10 @@ USE `forum`;
 
 -- Listage de la structure de table forum. category
 CREATE TABLE IF NOT EXISTS `category` (
-  `id_category` int NOT NULL,
+  `id_category` int NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   PRIMARY KEY (`id_category`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
 -- Listage des données de la table forum.category : ~6 rows (environ)
 INSERT INTO `category` (`id_category`, `title`) VALUES
@@ -37,49 +37,53 @@ INSERT INTO `category` (`id_category`, `title`) VALUES
 
 -- Listage de la structure de table forum. post
 CREATE TABLE IF NOT EXISTS `post` (
-  `id_post` int NOT NULL,
+  `id_post` int NOT NULL AUTO_INCREMENT,
   `text` text NOT NULL,
   `dateCrea` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `id_topic` int NOT NULL,
-  `id_user` int NOT NULL,
+  `user_id` int NOT NULL,
+  `topic_id` int NOT NULL,
   PRIMARY KEY (`id_post`),
-  KEY `FK_post_topic` (`id_topic`),
-  KEY `FK_post_user` (`id_user`),
-  CONSTRAINT `FK_post_topic` FOREIGN KEY (`id_topic`) REFERENCES `topic` (`id_topic`),
-  CONSTRAINT `FK_post_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+  KEY `user_id` (`user_id`),
+  KEY `topic_id` (`topic_id`),
+  CONSTRAINT `FK_post_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`),
+  CONSTRAINT `FK_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- Listage des données de la table forum.post : ~0 rows (environ)
 
 -- Listage de la structure de table forum. topic
 CREATE TABLE IF NOT EXISTS `topic` (
-  `id_topic` int NOT NULL,
+  `id_topic` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `dateCrea` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `locked` tinyint(1) NOT NULL,
-  `id_category` int NOT NULL,
-  `id_user` int NOT NULL,
+  `user_id` int NOT NULL DEFAULT '0',
+  `category_id` int NOT NULL,
   PRIMARY KEY (`id_topic`),
-  KEY `FK_topic_user` (`id_user`),
-  KEY `FK_topic_category` (`id_category`),
-  CONSTRAINT `FK_topic_category` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`),
-  CONSTRAINT `FK_topic_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  KEY `user_id` (`user_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `FK_topic_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
+  CONSTRAINT `FK_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
--- Listage des données de la table forum.topic : ~0 rows (environ)
+-- Listage des données de la table forum.topic : ~3 rows (environ)
+INSERT INTO `topic` (`id_topic`, `title`, `dateCrea`, `locked`, `user_id`, `category_id`) VALUES
+	(3, 'Batman', '2023-07-17 09:18:38', 1, 2, 1),
+	(4, 'Superman\r\n', '2023-07-17 09:19:07', 1, 1, 1),
+	(5, 'Spirou', '2023-07-17 09:19:33', 1, 3, 4);
 
 -- Listage de la structure de table forum. user
 CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int NOT NULL,
+  `id_user` int NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(50) NOT NULL,
   `pasword` varchar(255) NOT NULL,
   `dateRegistration` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
--- Listage des données de la table forum.user : ~0 rows (environ)
+-- Listage des données de la table forum.user : ~3 rows (environ)
 INSERT INTO `user` (`id_user`, `pseudo`, `pasword`, `dateRegistration`, `email`, `role`) VALUES
 	(1, 'lulu24', 'azerty', '2023-07-11 14:44:48', 'azerty@mail.fr', 'membre'),
 	(2, 'antho67', 'azerty', '2023-07-11 14:46:12', 'azerty@mail.fr', 'admin'),
