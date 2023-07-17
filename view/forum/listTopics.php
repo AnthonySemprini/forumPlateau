fb16235475c9bf1ef19853c24ea5467ea220c367
Anthony <semprini.anthony@gmail.com>
2023-07-17 14:17:32 +0200

    Affichage des topics par categorie

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -1,18 +1,26 @@
 <?php
 
 $topics = $result["data"]['topics'];
-    
+//var_dump($topics);die;
+  if(isset($topics)){
 ?>
 
 <h1>Liste des topics</h1>
 
 <?php
+    
 foreach($topics as $topic ){
-    ?>
+        
+        ?>
     <a href='index.php?ctrl=forum&action=listPosts'><p><?=$topic->getTitle()?></p></a>
     <?php
+
+
 }
-// var_dump($topic);die;
+}else{
+
+    echo "Cette categorie est vide";
+}
 
 
   
